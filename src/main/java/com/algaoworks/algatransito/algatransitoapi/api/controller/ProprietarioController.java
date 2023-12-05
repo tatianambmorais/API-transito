d11e942084ec23f94f1b2e6cfb317565bfda3cea
tatianambmorais <tatianambmorais@gmail.com>
2023-12-05 07:03:42 -0300

    Injetando e usando o repositório do SDJ

--- src/main/java/com/algaoworks/algatransito/algatransitoapi/api/controller/ProprietarioController.java
+++ src/main/java/com/algaoworks/algatransito/algatransitoapi/api/controller/ProprietarioController.java
@@ -1,9 +1,12 @@
 package com.algaoworks.algatransito.algatransitoapi.api.controller;
 
 import com.algaoworks.algatransito.algatransitoapi.domain.model.Proprietario;
+import com.algaoworks.algatransito.algatransitoapi.domain.repository.ProprietarioRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.TypedQuery;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,14 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Arrays;
 import java.util.List;
 
+@AllArgsConstructor
 @RestController
 public class ProprietarioController {
-    @PersistenceContext
-    private EntityManager manager;
+    private final ProprietarioRepository proprietarioRepository;
+
+
     @GetMapping("/proprietarios")
     public List<Proprietario> listar(){
-        TypedQuery<Proprietario> query=
-    manager.createQuery("from Proprietario", Proprietario.class);
-        return query.getResultList();
+     return proprietarioRepository.findAll();
     }
 }
--- src/main/java/com/algaoworks/algatransito/algatransitoapi/domain/repository/ProprietarioRepository.java
+++ src/main/java/com/algaoworks/algatransito/algatransitoapi/domain/repository/ProprietarioRepository.java
@@ -5,8 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class ProprietarioRepository {
+
     public interface ProprietarioRepository extends JpaRepository<Proprietario, Long>{
 
     }
-}
+
